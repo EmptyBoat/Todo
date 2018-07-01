800e3574539c78aadae936ed9657dc042ad8e83e
Pattarakan Phompao <boat.pattarakan@icloud.com>
2018-07-01 10:12:28 +0700

    new json

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 class ListItems extends Component {
-    
+
     render() {
         return (
-        <React.Fragment>
-            {this.props.todos.map(item =>(
+            <React.Fragment>
+                {this.props.todos.map(item => (
 
-            
-           
-            <div className="lish-checkbox" key={item.id}>
-                
-                <input type="checkbox"/>
-                {item.name}
-            </div>
 
-            ))}
-            
-        </React.Fragment>
+
+                    <div className="lish-checkbox" key={item.id}>
+
+                        <input type="checkbox" />
+                        {item.name}
+                    </div>
+
+                ))}
+
+            </React.Fragment>
 
         );
     }
